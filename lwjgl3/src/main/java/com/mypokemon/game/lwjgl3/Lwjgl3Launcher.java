7b5b52069a2e728415ea624dc1275014c560b6eb
DanielaGomez28 <ddgomez.25@est.ucab.edu.ve>
2025-12-28 23:48:31 -0400

    al fin

--- lwjgl3/src/main/java/com/mypokemon/game/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/mypokemon/game/lwjgl3/Lwjgl3Launcher.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mypokemon.game.PokemonMain;
 
 //ESTÚPIDA
+//TU 
 /** Launches the desktop (LWJGL3) application. */
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
